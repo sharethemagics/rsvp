286838f9b43eaa55a4b325bca16accef764366ed
Sathees Rajagopal <sharethemagics@gmail.com>
2018-04-03 12:55:23 +0530

    first commit

---
+++ server/config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  AUTH0_DOMAIN: 'https://projmanagement.auth0.com', // e.g., kmaida.auth0.com
+  AUTH0_API_AUDIENCE: 'http://localhost:8083/api/', // e.g., 'http://localhost:8083/api/'
+  //MONGO_URI: process.env.MONGO_URI || 'mongodb://[USER]:[PASSWORD]@[DS######].mlab.com:[PORT]/[DB_NAME]'
+};
